be34096b7e3ba6e87997e906524b0cc62b844908
Samuel D'souza <samuel_dsouza@outlook.com>
2025-12-16 20:50:51 +0000

    implemented an out of bound checker.

--- TTT/src/main/TUI.java
+++ TTT/src/main/TUI.java
@@ -6,30 +6,36 @@ import java.util.Scanner;
 
 public final class TUI {
 	private static Grid grid;
+	private final static int GRIDSIZE = 3;
 	
-	public static void start() {	
-		grid = new Grid(3);//creates a 3x3 grid
+	public static void start() {
+		grid = new Grid(GRIDSIZE);//creates a 3x3 grid
 
+		//The game loop
 		while(true) {
 			turn();
 		}
 	}
 	public static void turn() {
-		System.out.println(grid);
 		
-		String row = "";
-		String collumn = "";
+		int row = 0;
+		int collumn = 0;
 		
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.println("Enter row");
-		row = sc.next();
+		row = sc.nextInt();
 		
 		System.out.println("Enter collumn");
-		collumn = sc.next();
+		collumn = sc.nextInt();
 		
-		grid.selectCell(Integer.parseInt(row), Integer.parseInt(collumn));//parse to integer!!!
-		
-		System.out.println(grid);
+		//index out of bounds check
+		if(row > GRIDSIZE || row <= 0 || collumn > GRIDSIZE || collumn <= 0) {
+			System.out.println("Out of bounds. Please try again");
+		}
+		else {
+			grid.selectCell(row, collumn);
+			System.out.println(grid);
+		}
 	}
 }
