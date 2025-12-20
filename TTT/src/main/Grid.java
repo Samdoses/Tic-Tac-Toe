c297c52e6d83a5d5d66adf7d53c5d2580a5c50cc
Samuel D'souza <samuel_dsouza@outlook.com>
2025-12-20 10:44:09 +0000

    first shot at this

--- TTT/src/main/Grid.java
+++ TTT/src/main/Grid.java
@@ -1,16 +1,15 @@
 package main;
 
 public class Grid {
-	Cell[][] grid;
+	int[][] grid;
 	//make size a 
 	
 	public Grid(int size) {
-		grid = new Cell[size][size];
+		grid = new int[size][size];
 		
 		for(int row=0;row<size;row++) {
 			for(int collumn=0; collumn<size;collumn++) {
-				Cell cell = new Cell(row, collumn);
-				grid[row][collumn] = cell;	
+				grid[row][collumn] = 0;	
 			}
 		}
 	}
