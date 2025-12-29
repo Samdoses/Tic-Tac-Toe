c1984b7ff33a2139173d60c3598fe17acc297e47
Samuel D'souza <samuel_dsouza@outlook.com>
2025-12-29 10:11:58 +0000

    removed printing opponent's action after each turn

--- TTT/src/main/RandomComputer.java
+++ TTT/src/main/RandomComputer.java
@@ -13,12 +13,9 @@ public class RandomComputer implements Computer {
 	
 	public void selectCell() {
 		Random random = new Random();
-		//CHANGE LATER TO SIZE
+		
 		int row = random.nextInt(gridSize) + 1;
 		int collumn = random.nextInt(gridSize) + 1;
-		
-		System.out.println(row);
-		System.out.println(collumn);
 		
 		grid.selectCell(row, collumn, grid.getOpponent());
 	}
