85efcef569b4345489df1f2b1c72c6de82880a28
Samuel D'souza <samuel_dsouza@outlook.com>
2026-01-28 20:43:59 +0000

    added java fx to the project

--- TTT/src/main/Main.java
+++ TTT/src/main/Main.java
@@ -1,10 +1,31 @@
 package main;
 
-public class Main {
-	
-	
-	public static void main(String[] args) {
-			TUI.start();
-	}
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
 
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        // Simple UI test
+        Label label = new Label("Tic-Tac-Toe with JavaFX!");
+        StackPane root = new StackPane(label);
+        Scene scene = new Scene(root, 300, 250);
+
+        primaryStage.setTitle("Tic Tac Toe");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+        
+        // Note: You can call TUI.start() here if you want to run the console version 
+        // in a separate thread, but usually you replace TUI with the GUI.
+    }
+
+    public static void main(String[] args) {
+        // Launch the JavaFX application
+        launch(args);
+        TUI.start();
+    }
 }
--- TTT/src/module-info.java
+++ TTT/src/module-info.java
@@ -5,4 +5,12 @@
  * 
  */
 module TTT {
+	// Requires the JavaFX controls module
+    requires javafx.controls;
+    
+    // (Optional) If you plan to use FXML
+    requires javafx.fxml;
+
+    // You must export your 'main' package so JavaFX can access your Application class
+    exports main;
 }
